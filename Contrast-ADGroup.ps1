a33f5935e43b9bf6ae6ddf2cdb4ecac0e5b77b85
Croisade <jamalgardiner7@live.com>
2020-02-09 11:17:53 -0500

    Initial commit of Contrast-ADGroup

---
+++ Contrast-ADGroup.ps1
@@ -0,0 +1,16 @@
+﻿Function Contrast-ADGroup{
+      [cmdletbinding()]
+      param(
+          [Parameter(Mandatory)]
+          [string]$Primary,
+
+          [Parameter(Mandatory)]
+          [string]$Secondary
+      )
+    Write-Host "$Primary is on the left, $Secondary is on the right"
+    Compare-Object -ReferenceObject (Get-AdPrincipalGroupMembership $primary | select name | sort-object -Property name) -DifferenceObject (Get-AdPrincipalGroupMembership $Secondary | select name | sort-object -Property name) -property name -passthru
+
+    
+}
+
+
