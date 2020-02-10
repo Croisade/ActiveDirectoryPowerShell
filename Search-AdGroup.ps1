e378980b4423908ce4888d799b15599d3d0ec85f
Croisade <jamalgardiner7@live.com>
2020-02-10 12:34:39 -0500

    Initial Commit

---
+++ Search-AdGroup.ps1
@@ -0,0 +1,9 @@
+﻿function Search-AdGroup{
+    [cmdletbinding()]
+    param(
+    [parameter(Mandatory)]
+    [string]$Group
+    )
+    Get-ADGroup -Filter {Name -like $Group}  | Select-Object -Property SamAccountName
+}
+
